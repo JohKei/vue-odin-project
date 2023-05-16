415c72194d4093d90c37391511e814e4ee1c8faf
John <kaebjohann@googlemail.com>
2023-05-16 17:32:42 +0200

    create project

---
+++ src/views/odinForm.vue
@@ -0,0 +1,19 @@
+<template>
+    <form>
+        <label for="choose">Would you prefer a banana or a cherry?</label>
+        <input id="choose" name="i_like">
+        <button>Submit</button>
+    </form>
+</template>
+
+
+<script lang="ts" setup>
+
+
+
+</script>
+
+<style scoped lang="css">
+
+
+</style>
