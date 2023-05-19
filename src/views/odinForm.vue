9bdf0536a0d8ea0d09e258e7ecba62f5c9fd3412
John <kaebjohann@googlemail.com>
2023-05-19 15:58:29 +0200

    add basic form inputs add email Validation

--- src/views/odinForm.vue
+++ src/views/odinForm.vue
@@ -7,12 +7,65 @@
             </div>
         </div>
         <div class="rightSide">
-
+            <h1 class="heading">Sign up for The Odin Project <em>today!</em></h1>
+            <p>
+                This is not a real online service! You know you need something like this in your life to
+                help you realize your deepest dreams. Sign up <em>now</em> to get started.
+            </p>
+            <p>
+                You know you want to.
+            </p>
+            <div class="formContainer">
+                <Form @submit="onSubmit" class="form" id="odinForm">
+                    <label for="firstName">
+                        First Name: *
+                        <Field type="text" name="firstName" id="firstName"/>
+                    </label>
+                    <label for="lastName">
+                        Last Name: *
+                        <Field type="text" name="lastName" id="lastName"/>
+                    </label>
+                    <label for="mail">
+                        Email: *
+                        <Field type="email" name="mail" id="mail" :rules="veeValidate"/>
+                    </label>
+                    <label for="phoneNumber">
+                        Phone Number:
+                        <Field type="text" name="phoneNumber" id="phoneNumber" />
+                    </label>
+                    <label for="password">
+                        Password
+                        <Field type="password" name="password" id="password"/>
+                    </label>
+                    <label for="confirmPassword">
+                        confirm Password
+                        <Field type="password" name="confirmPassword" id="confirmPassword"/>
+                    </label>
+                </Form>
+            </div>
+            <button type="submit" form="odinForm">Create Account</button>
+            <p>Already have an account? <a href="#">Log in</a></p>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import {Form, Field} from 'vee-validate'
+
+const onSubmit = (values: object) => {
+    console.log(values)
+}
+
+const veeValidate = (value: string) => {
+    if (!value) {
+        return 'This field is required'
+    }
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    if (!regex.test(value)) {
+        return 'This field must be a valid email';
+    }
+    return true
+}
 
 </script>
 
@@ -34,17 +87,21 @@
 }
 
 .rightSide {
-    flex: 1 1 300px;
+    flex: 2 1 300px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 3rem;
 }
 
 
-.logoContainer{
+.logoContainer {
     height: 200px;
     display: flex;
     align-items: center;
     width: 100%;
     justify-content: center;
-    background-color: rgba(0, 0, 0,50%);
+    background-color: rgba(0, 0, 0, 50%);
 }
 
 .slogan {
@@ -52,10 +109,29 @@
     color: white;
     display: inline-block;
     margin: 0;
-    font-size: 30px;
+    font-size: 2rem;
 }
-.odinLogo{
-    height: 150px;
+
+.odinLogo {
+    height: 10rem;
     width: auto;
 }
+
+.heading {
+    font-size: 1.5em;
+    font-family: Norse-Bold, serif;
+}
+.formContainer{
+    margin: 3rem 0;
+}
+.form {
+    //margin: 5rem 0;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 10px;
+}
 </style>
+<!--todo fill-in progress bar-->
+<!--todo if inputs valid green border around input fields-->
+<!--todo if Passwords not equal -> red border-->
