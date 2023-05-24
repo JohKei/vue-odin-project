166b7153dca2403abc281996b894319ea8a52438
John <kaebjohann@googlemail.com>
2023-05-24 22:08:28 +0200

    improve navbar toggle with vue add Todo for tomorrow

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="customMenu">
-    <img src="../assets/menu.svg" alt="Menu Icon" class="menuIcon" @click="toggleNav">
-    <div :class="{customNavbar: navActive, customNavbarNotActive: navNotActive}">
+    <img src="../assets/menu.svg" alt="Menu Icon" class="menuIcon" @click="navActive = !navActive">
+    <div v-if="navActive" class="customNavbar">
       <router-link to="/">Home</router-link>
       <router-link to="#">About</router-link>
       <div class="odinProjectsDropdownContainer">
-        <div class="odinDropdownBtn">
+        <button class="odinDropdownBtn" @click="odinContentActive = !odinContentActive">
           Odin Projects
           <i class="arrow down"></i>
-        </div>
-        <div class="odinDropContent">
+        </button>
+        <div v-if="odinContentActive" class="odinDropContent">
           <router-link to="/odinForm">Odin Form</router-link>
           <router-link to="/odinDashboard">Odin Dashboard</router-link>
         </div>
       </div>
       <div class="personalPlayGroundDropContainer">
-        <div class="personalDropBtn">
+        <button class="personalDropBtn" @click="personalContentActive = !personalContentActive">
           Personal Playground
           <i class="arrow down"></i>
-        </div>
-        <div class="personalDropContent">
+        </button>
+        <div v-if="personalContentActive" class="personalDropContent">
           <router-link to="/odinPractice">
             Odin Practice
           </router-link>
@@ -30,50 +30,44 @@
       </div>
     </div>
   </div>
-
+<!--todo just make the OdinProjects & Personal playground dropdowns always active. Just style it little bit smaller-->
 </template>
 
-<!--todo Dropdown The Odin Project projects & personal playground-->
 <script setup lang="ts">
 import {ref} from "vue";
 
 const navActive = ref(false)
-const navNotActive = ref(true)
-const toggleNav = () => {
-  navActive.value = !navActive.value
-  navNotActive.value = !navNotActive.value
-}
+const odinContentActive = ref(false)
+const personalContentActive = ref(false)
+
+
 </script>
 
-
 <style scoped lang="css">
-.menuIcon{
+.menuIcon {
   height: auto;
   width: 50px;
 }
-.customMenu{
+
+.customMenu {
   position: absolute;
-  background-color: rgba(255,255,255, 30%);
+  background-color: rgba(255, 255, 255, 30%);
   border-radius: 10px;
   --headerHeight: 50px;
   height: var(--headerHeight);
 }
-.customNavbarNotActive {
-  display: none;
-  gap: 10px;
-  background-color: rgba(0,0,0, 50%);
-}
-.customNavbar{
+
+.customNavbar {
   display: flex;
   gap: 10px;
-  background-color: rgba(0,0,0, 50%);
+  background-color: rgba(0, 0, 0, 50%);
   flex-direction: column;
   width: 30vh;
   align-items: center;
   height: calc(100vh - var(--headerHeight));
 }
 
-.customNavbar a{
+.customNavbar a {
   color: white;
   text-decoration: none;
 }
@@ -92,29 +86,32 @@ const toggleNav = () => {
 }
 
 
-.odinProjectsDropdownContainer:hover .odinDropContent {
+.odinDropContent {
   display: flex;
   flex-direction: column;
   background-color: inherit;
   align-items: center;
 }
 
-.personalPlayGroundDropContainer:hover .personalDropContent {
+.personalDropContent {
   display: flex;
   flex-direction: column;
   background-color: inherit;
   align-items: center;
 }
-.odinProjectsDropdownContainer:hover .odinDropContent a,
-.personalPlayGroundDropContainer:hover .personalDropContent a{
+
+.odinDropContent a,
+.personalDropContent a {
   color: white;
 }
+
 .arrow {
   border: solid white;
   border-width: 0 3px 3px 0;
   display: inline-block;
   padding: 3px;
 }
+
 .down {
   transform: rotate(45deg);
   -webkit-transform: rotate(45deg);
