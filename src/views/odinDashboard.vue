f5ff4a84ee3be315e82087c2ef4a79c7af845a58
John <kaebjohann@googlemail.com>
2023-05-24 11:19:07 +0200

    refactor Sidebar p elements into vue router-link. finish Sidebar

--- src/views/odinDashboard.vue
+++ src/views/odinDashboard.vue
@@ -12,41 +12,41 @@
       <div class="sidebarContent">
         <div>
           <img src="../assets/home-circle-outline.svg" alt="Home Icon">
-          <p>Home</p>
+          <router-link to="/odinDashboard">Home</router-link>
         </div>
         <div>
           <img src="../assets/card-account-details-outline.svg" alt="Profile Icon">
-          <p>Profile</p>
+          <router-link to="#">Profile</router-link>
         </div>
         <div>
           <img src="../assets/message.svg" alt="Messages Icon">
-          <p>Messages</p>
+          <router-link to="#">Messages</router-link>
         </div>
         <div>
           <img src="../assets/clock-time-three-outline.svg" alt="History Icon">
-          <p>History</p>
+          <router-link to="#">History</router-link>
         </div>
         <div>
           <img src="../assets/file-multiple.svg" alt="Tasks Icon">
-          <p>Tasks</p>
+          <router-link to="#">Tasks</router-link>
         </div>
         <div>
           <img src="../assets/account-group.svg" alt="Communities Icon">
-          <p>Communities</p>
+          <router-link to="#">Communities</router-link>
         </div>
       </div>
       <div class="sidebarSettings">
         <div>
           <img src="../assets/cog.svg" alt="Settings Icon">
-          <p>Settings</p>
+          <router-link to="#">Settings</router-link>
         </div>
         <div>
           <img src="../assets/chat-question.svg" alt="Support Icon">
-          <p>Content</p>
+          <router-link to="#">Content</router-link>
         </div>
         <div>
           <img src="../assets/shield-account.svg" alt="Privacy Icon">
-          <p>Privacy</p>
+          <router-link to="#">Privacy</router-link>
         </div>
       </div>
     </div>
@@ -76,8 +76,6 @@ import NavigationBar from "@/components/NavigationBar.vue";
   display: grid;
   grid-template-rows: 1fr;
   grid-template-columns: 1fr 4fr;
-  justify-items: center;
-  padding-top: 10px;
 }
 .headerContainer {
   grid-row: 1/2;
@@ -95,7 +93,8 @@ import NavigationBar from "@/components/NavigationBar.vue";
   grid-template-columns: 1fr;
 }
 
-.sidebarContainer > div > div {
+.sidebarContainer > div > div,
+.sidebarHeading{
   display: grid;
   grid-template-columns: 1fr 4fr;
   justify-items: center;
@@ -107,12 +106,14 @@ import NavigationBar from "@/components/NavigationBar.vue";
   height: auto;
   width: 40px;
 }
-.sidebarContent > div > p,
-.sidebarSettings > div > p,
+.sidebarContent > div > a,
+.sidebarSettings > div > a,
 .sidebarHeading > h2{
   margin: 0;
   padding: 0;
   justify-self: start;
+  text-decoration: none;
+  color: black;
 }
 .contentContainer {
   grid-column: 2/3;
