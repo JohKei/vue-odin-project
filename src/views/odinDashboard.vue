3d1a151a43ee8443da1591c0283dd264df1f17ef
John <kaebjohann@googlemail.com>
2023-05-24 10:21:10 +0200

    justify Container placement

--- src/views/odinDashboard.vue
+++ src/views/odinDashboard.vue
@@ -1,5 +1,16 @@
 <template>
   <NavigationBar></NavigationBar>
+  <div class="rootContainer">
+    <div class="headerContainer">
+      <h1>Header Content</h1>
+    </div>
+    <div class="sidebarContainer">
+      <h1>short</h1>
+    </div>
+    <div class="contentContainer">
+      <h1>short</h1>
+    </div>
+  </div>
 </template>
 
 
@@ -10,5 +21,28 @@ import NavigationBar from "@/components/NavigationBar.vue";
 </script>
 
 <style scoped lang="css">
+.rootContainer {
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  grid-template-rows: 1fr 5fr;
+  height: 100vh;
+  width: 100vw;
+  padding-top: 20px;
+}
 
+.headerContainer {
+  grid-row: 1/2;
+  grid-column: 2/3;
+  border: solid black 1px;
+}
+.sidebarContainer{
+  grid-column: 1/2;
+  grid-row: 1/3;
+  border: solid black 1px;
+}
+.contentContainer{
+  grid-column: 2/3;
+  grid-row: 2/3;
+  border: solid black 1px;
+}
 </style>
