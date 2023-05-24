1c744bb9295648f58c6de3200cfcb382d09123d3
John <kaebjohann@googlemail.com>
2023-05-24 10:41:20 +0200

    aligning sidebar placeholder Content

--- src/views/odinDashboard.vue
+++ src/views/odinDashboard.vue
@@ -5,7 +5,50 @@
       <h1>Header Content</h1>
     </div>
     <div class="sidebarContainer">
-      <h1>short</h1>
+      <div class="sidebarHeading">
+        <img src="#" alt="Dashboard Icon">
+        <h2>Dashboard</h2>
+      </div>
+      <div class="sidebarContent">
+        <div>
+          <p>Image</p>
+          <p>Content</p>
+        </div>
+        <div>
+          <p>Image</p>
+          <p>Content</p>
+        </div>
+        <div>
+          <p>Image</p>
+          <p>Content</p>
+        </div>
+        <div>
+          <p>Image</p>
+          <p>Content</p>
+        </div>
+        <div>
+          <p>Image</p>
+          <p>Content</p>
+        </div>
+        <div>
+          <p>Image</p>
+          <p>Content</p>
+        </div>
+      </div>
+      <div class="sidebarSettings">
+        <div>
+          <p>Image</p>
+          <p>Content</p>
+        </div>
+        <div>
+          <p>Image</p>
+          <p>Content</p>
+        </div>
+        <div>
+          <p>Image</p>
+          <p>Content</p>
+        </div>
+      </div>
     </div>
     <div class="contentContainer">
       <h1>short</h1>
@@ -29,18 +72,36 @@ import NavigationBar from "@/components/NavigationBar.vue";
   width: 100vw;
   padding-top: 20px;
 }
-
+.sidebarHeading{
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr 4fr;
+  justify-items: center;
+  padding-top: 10px;
+}
 .headerContainer {
   grid-row: 1/2;
   grid-column: 2/3;
   border: solid black 1px;
 }
-.sidebarContainer{
+
+.sidebarContainer {
   grid-column: 1/2;
   grid-row: 1/3;
   border: solid black 1px;
+  background-color: deepskyblue;
+  display: grid;
+  grid-template-rows: 1fr 3fr 5fr;
+  grid-template-columns: 1fr;
 }
-.contentContainer{
+
+.sidebarContainer > div > div {
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  justify-items: center;
+}
+
+.contentContainer {
   grid-column: 2/3;
   grid-row: 2/3;
   border: solid black 1px;
