f264c7d868cb98420c787e0817bb8e0f569862f5
John <kaebjohann@googlemail.com>
2023-05-25 19:47:25 +0200

    TOP js Course -> Objects and Object Constructors

--- src/views/odinPractice.vue
+++ src/views/odinPractice.vue
@@ -1,10 +1,36 @@
 <template>
   <navigation-bar></navigation-bar>
 </template>
+
+
 <script setup lang="ts">
 import NavigationBar from "@/components/NavigationBar.vue";
 
+
+class Book {
+  title: string;
+  author: string;
+  pages: number;
+  read: boolean;
+  info(){
+    return (`${this.title} by ${this.author}, ${this.pages} pages, readed it? ${this.read}`)
+  }
+  constructor(title:string, author:string, pages:number, read:boolean) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages
+    this.read = read
+
+  }
+}
+const gta = new Book("Grand Thef Auto 6", "Rockstar Games", 6, true)
+// console.log(gta.info())
+
+
+console.log(Object.getPrototypeOf(gta) === Book.prototype)
+
+
+
 </script>
 <style scoped lang="css">
-
 </style>
