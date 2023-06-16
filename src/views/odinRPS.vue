7c6a139f621461837de24a7b0924a44cf8a2cc93
John <kaebjohann@googlemail.com>
2023-06-16 13:25:16 +0200

    remove comments

--- src/views/odinRPS.vue
+++ src/views/odinRPS.vue
@@ -1,32 +1,23 @@
 <template>
   <navigation-bar></navigation-bar>
   <div class="body">
-    <!-- introPopup -->
     <div class="introPopup">
-
     </div>
-    <!-- gameEndPopup -->
     <div class="gameEndPopup" ref="gameOverDiv">
       <h1>{{ gameOverHel }}</h1>
       <p>{{ lastGameEl }}</p>
       <button @click="restartGame">restart game</button>
     </div>
-
     <div class="actualBody" ref="mainBodyEl">
-
-      <!-- header -->
       <div class="header">
         <h1 class="readability">Play Rock, Paper, Scissor vs. Villains!</h1>
       </div>
-
-      <!-- scoreBoard -->
       <div class="scoreBoard">
         <div class="userContainer">
           <h2 class="rM readability">ヒーロー</h2>
           <h2 class="readability rM">(hero)</h2>
           <h2 class="score rM readability">Player Score {{ userScore }}</h2>
         </div>
-
         <div class="gifContainer" id="gifContainer" ref="gifContainer">
           <img v-if="draw" src="../assets/odinRPS/draw.gif" alt="img" class="gif">
           <img v-if="computerPaper" src="../assets/odinRPS/computerPaper.gif" alt="img" class="gif">
@@ -36,31 +27,22 @@
           <img v-if="userRock" src="../assets/odinRPS/userRock.gif" alt="img" class="gif">
           <img v-if="userScissor" src="../assets/odinRPS/userScissor.gif" alt="img" class="gif">
         </div>
-
         <div class="villainContainer">
           <h2 class="rM readability">悪党</h2>
           <h2 class="readability rM">(villain)</h2>
           <h2 class="score rM readability">Computer Score {{ computerScore }}</h2>
         </div>
       </div>
-
-      <!-- description what happened this round -->
       <div class="descriptionContainer">
         <p id="roundStatus-el" class="description readability">{{ roundStatusEl }}</p>
-
       </div>
-
-      <!-- buttons -->
       <div class="buttonContainer">
         <div class="selectionContainer">
-          <!-- button active class for press animation -->
           <img src="../assets/odinRPS/rock.png" class="rockBtn" alt="rock" ref="rockBtn" @click="rockF">
           <img src="../assets/odinRPS/paper.png" class="paperBtn" alt="paper" ref="paperBtn" @click="paperF">
           <img src="../assets/odinRPS/scissor.png" class="scissorBtn" alt="scissor" ref="scissorBtn" @click="scissorF">
         </div>
       </div>
-
-      <!-- footer -->
       <div class="footer">
         <p class="readability">
           <a href="https://github.com/JohKei/odinRockPaperScissor" target="_blank"
@@ -74,9 +56,6 @@
 </template>
 
 <script setup lang="js">
-
-//Logic Section
-
 import {ref} from "vue";
 import NavigationBar from "@/components/NavigationBar.vue";
 
@@ -96,11 +75,6 @@ function getComputerChoice() {
   return computerSelection = selections[Math.floor(Math.random() * 3)]
 }
 
-//playRound function which takes playerSelection as parameter
-//this function should return:
-//a winner with a string like "You Lose! Rock beats Scissor"
-//or "It's a Draw!"
-
 const playRound = async (user) => {
   let computer = getComputerChoice(computerSelection)
   draw.value = false
@@ -111,62 +85,43 @@ const playRound = async (user) => {
   userRock.value = false
   userScissor.value = false
   if (user === computer) {
-    // createAppendGif("../assets/odinRPS/draw.gif")
     roundStatusEl.value = `It's a Draw! You choose ${user} and the computer choose ${computer}!`
-    draw.value= true
+    draw.value = true
   } else if (user === "rock" && computer === "paper") {
     roundStatusEl.value = `You Lost! you chose ${user} and the computer chose ${computer}`
     computerScore.value += 1
-    // updateScoreEl()
     checkWinner()
-    // if (computerScore.value < 5) {
-    //   await showGif("paper", "computer")
-    // }
-    computerPaper.value= true
+
+    computerPaper.value = true
   } else if (user === "rock" && computer === "scissor") {
     roundStatusEl.value = `You Won! you chose ${user} and the computer chose ${computer}`
     userScore.value += 1
-    // updateScoreEl()
     checkWinner()
-    // if (userScore.value < 5) {
-    //   showGif("rock", "user")
-    // }
+
     userRock.value = true
   } else if (user === "paper" && computer === "rock") {
     roundStatusEl.value = `You Won! you chose ${user} and the computer chose ${computer}`
     userScore.value += 1
-    // updateScoreEl()
     checkWinner()
-    // if (userScore.value < 5) {
-    //   showGif("paper", "user")
-    // }
-    userPaper.value= true
+
+    userPaper.value = true
   } else if (user === "paper" && computer === "scissor") {
     roundStatusEl.value = `You Lost! you chose ${user} and the computer chose ${computer}`
     computerScore.value += 1
-    // updateScoreEl()
     checkWinner()
-    // if (computerScore.value < 5) {
-    //   showGif("scissor", "computer")
-    // }
+
     computerScissor.value = true
   } else if (user === "scissor" && computer === "rock") {
     roundStatusEl.value = `You Lost! you chose ${user} and the computer chose ${computer}`
     computerScore.value += 1
-    // updateScoreEl()
     checkWinner()
-    // if (computerScore.value < 5) {
-    //   showGif("rock", "computer")
-    // }
+
     computerRock.value = true
   } else if (user === "scissor" && computer === "paper") {
     roundStatusEl.value = `You Won! you chose ${user} and the computer chose ${computer}`
     userScore.value += 1
-    // updateScoreEl()
     checkWinner()
-    // if (userScore.value < 5) {
-    //   showGif("scissor", "user")
-    // }
+
     userScissor.value = true
   }
 }
@@ -177,33 +132,21 @@ function checkWinner() {
     let message = (`You reached ${userScore.value} Points while the Villain reached ${computerScore.value} Points!`)
     lastGame(message)
     gameOver()
-    // callBackGif()
   } else if (computerScore.value === 5) {
     gameOverHel.value = `Game Over!`
     let message = (`You Lost! you reached ${userScore.value} Points while the Villain reached ${computerScore.value} Points!`)
     lastGame(message)
     gameOver()
-    // callBackGif()
   }
 }
 
-
-//Intro Popup Section
-
-//Outro Popup Section
-// const gameOverDiv = document.querySelector("#gameOver")
 const gameOverDiv = ref()
-// const gameOverHEl = document.querySelector("#gameOver-el")
 const gameOverHel = ref('')
-// const lastGameEl = document.querySelector("#lastGame-el")
 const lastGameEl = ref()
-// const restartBtn = document.querySelector("#restart-btn")
 
-//gameOver Popup function
 function lastGame(x) {
   lastGameEl.value = x
 }
-
 
 const mainBodyEl = ref()
 
@@ -212,95 +155,27 @@ function gameOver() {
   mainBodyEl.value.style.visibility = "hidden"
 }
 
-//Header Section
-
-//ScoreBoard Section
-// const playerScoreEl = document.querySelector("#playerScore-el")
-// const computerScoreEl = document.querySelector("#computerScore-el")
-
 const gifContainer = ref()
-// const gifImg = ref()
-// const imgSource = ref('')
-
-//gifContainer Section
-// function createAppendGif(x) {
-  // // const gifImg = document.createElement("img");
-  // gifImg.value.classList.add("gif")
-  // gifImg.value.setAttribute("id", "gif")
-  // gifImg.value.src = `${x}  ${Date.now()}`;
-  // // gifContainer.value.appendChild(gifImg.value)
-  // gifImg.value.src = x
-  // console.log(gifImg.value)
-// }
-
-
-// function callBackGif() {
-//   const gifContainer = document.querySelector("#gifContainer");
-//   gifContainer.innerHTML = "";
-// }
-
-
-//function show rock gif if rock wins the round
-// const showGif = (whatGif, whatWinner) => {
-//   if (whatGif === "rock" && whatWinner === "computer") {
-//     // createAppendGif("../assets/odinRPS/computerRock.gif")
-//     computerRock.value = true
-//   } else if (whatGif === "scissor" && whatWinner === "computer") {
-//     // createAppendGif("../assets/odinRPS/computerScissor.gif")
-//     computerScissor.value = true
-//   } else if (whatGif === "paper" && whatWinner === "computer") {
-//     // createAppendGif("../assets/odinRPS/computerPaper.gif")
-//     computerPaper.value = true
-//   } else if (whatGif === "rock" && whatWinner === "user") {
-//     // createAppendGif("../assets/odinRPS/userRock.gif")
-//     userRock.value = true
-//   } else if (whatGif === "scissor" && whatWinner === "user") {
-//     // createAppendGif("../assets/odinRPS/userScissor.gif")
-//
-//   } else if (whatGif === "paper" && whatWinner === "user") {
-//     // createAppendGif("../assets/odinRPS/userPaper.gif")
-//     userPaper.value = true
-//   }
-// }
-
-
-//description Section
-// const roundStatusEl.value = document.querySelector("#roundStatus-el")
 const roundStatusEl = ref('choose your Weapon')
-//Button Section
-// const rockBtn = document.querySelector("#rock-btn")
 const rockBtn = ref()
-// const paperBtn = document.querySelector("#paper-btn")
 const paperBtn = ref()
-// const scissorBtn = document.querySelector("#scissor-btn")
 const scissorBtn = ref()
-
-//when pressed rock,paper,scissor run playRound(selection)
-
-// rockBtn.addEventListener("click", function () {
 const rockF = () => {
   // callBackGif()
   playRound(selections[0])
   btnAnimation(rockBtn)
 }
-
-// paperBtn.addEventListener("click", function () {
 const paperF = () => {
   // callBackGif()
   playRound(selections[1])
   btnAnimation(paperBtn)
 }
-// scissorBtn.addEventListener("click", function () {
 const scissorF = () => {
   // callBackGif()
   playRound(selections[2])
   btnAnimation(scissorBtn)
 }
-// restartBtn.addEventListener("click", function () {
-//   restartGame()
-// })
 
-//restartGame
 function restartGame() {
   draw.value = false
   computerPaper.value = false
@@ -317,16 +192,12 @@ function restartGame() {
   roundStatusEl.value = "Choose your Weapon"
 }
 
-/* button click animation */
 function btnAnimation(x) {
   x.value.classList.add("buttonActive")
   setTimeout(() => {
     x.value.classList.remove("buttonActive")
   }, 150);
 }
-
-//footer Section
-
 </script>
 
 <style scoped lang="css">
@@ -358,7 +229,6 @@ function btnAnimation(x) {
   width: fit-content;
 }
 
-/* header */
 .header {
   width: 100%;
   text-align: center;
@@ -367,7 +237,6 @@ function btnAnimation(x) {
   justify-content: center;
 }
 
-/* Score Board */
 .scoreBoard {
   width: 70%;
   display: flex;
@@ -396,7 +265,6 @@ function btnAnimation(x) {
   border-radius: 10px;
 }
 
-/* Button Container */
 .buttonContainer {
   width: 100%;
 }
@@ -425,7 +293,6 @@ function btnAnimation(x) {
   width: 200px;
 }
 
-/* Description */
 .description {
   font-size: 25px;
   color: black;
@@ -435,13 +302,11 @@ function btnAnimation(x) {
   display: flex;
 }
 
-/* footer */
 .repoLink {
   color: black;
   text-decoration: none;
 }
 
-/* intro Popup */
 .introPopup {
   position: fixed;
   width: 100%;
@@ -451,7 +316,6 @@ function btnAnimation(x) {
   visibility: hidden;
 }
 
-/* Game Over PopUp */
 .gameEndPopup {
   position: fixed;
   width: 100%;
@@ -478,6 +342,5 @@ function btnAnimation(x) {
   width: 200px;
   height: 40px;
   border-radius: 10px;
-
 }
 </style>
