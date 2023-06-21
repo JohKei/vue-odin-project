b94f1ebbde7249c55e12cb314ab3dc4f67c9759d
John <kaebjohann@googlemail.com>
2023-06-21 13:01:19 +0200

    delete old Code

--- src/views/odinPractice.vue
+++ src/views/odinPractice.vue
@@ -1,21 +1,6 @@
 <template>
   <navigation-bar></navigation-bar>
   <div class="practiceBody">
-    <div>
-      <h1>People</h1>
-      <input type="text" v-model="people.input.value">
-      <button @click="people.addPerson">Add Person</button>
-      <ul>
-        <li v-for="person in people.array" :key="person">
-          <div>
-            <span>{{ person }}</span>
-            <button @click="people.removePerson(person)">x</button>
-          </div>
-        </li>
-      </ul>
-    </div>
-    <button @click="people.logOut()">Log</button>
-    <h2>Stats: {{ stats.setPeople.value }}</h2>
 
   </div>
 </template>
@@ -23,56 +8,6 @@
 
 <script setup lang="ts">
 import NavigationBar from "@/components/NavigationBar.vue";
-import {computed, reactive, ref} from "vue";
-
-// Revealing Modular pattern
-const people = (function () {
-  const input = ref('')
-  const _state = reactive({
-    people: ['Will', 'Smith'],
-  })
-
-  const peopleArr = computed(() => _state.people)
-  const addPerson = () => {
-    _state.people.push(input.value)
-    input.value = ''
-  }
-  const removePerson = (who: string) => {
-    const personIndex = _state.people.indexOf(who)
-    if (personIndex !== -1) {
-      _state.people.splice(personIndex, 1)
-    }
-  }
-
-  const log = () => {
-    console.log(peopleArr.value)
-  }
-
-  return {
-    array: peopleArr.value,
-    logOut: log,
-    addPerson,
-    removePerson,
-    input
-  }
-})()
-
-const stats = (function () {
-  const setPeople = computed(() => people.array.length)
-
-  return {setPeople}
-})()
-
-const human = {
-  species: 'Human',
-  create: function (values:any){
-    const instance = Object.create(this)
-    Object.keys(values).forEach(function (key){
-      instance[key] = values[key]
-    })
-    return instance
-  },
-}
 
 </script>
 
