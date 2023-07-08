9d3d4c80621cadf512a15cd6683d02f9969eb517
John <kaebjohann@googlemail.com>
2023-07-08 11:19:20 +0200

    add component-views & transitions edit componentBody's to be position absolute

--- src/views/RestaurantPage.vue
+++ src/views/RestaurantPage.vue
@@ -1,10 +1,18 @@
 <template>
   <restaurant-nav
-  :active-view="activeView"
-  @change-view="toggleView"
+      :active-view="activeView"
+      @change-view="toggleView"
   />
   <div class="restaurantBody">
-
+    <Transition>
+      <restaurant-home v-if="activeView[0]" class="home"/>
+    </Transition>
+    <Transition>
+      <restaurant-menu v-if="activeView[1]" class="menu"/>
+    </Transition>
+    <Transition>
+      <restaurant-contact v-if="activeView[2]" class="contact"/>
+    </Transition>
   </div>
 </template>
 
@@ -12,6 +20,9 @@
 <script setup lang="ts">
 import RestaurantNav from "../components/restaurant Page/restaurantNav.vue";
 import {ref} from "vue";
+import RestaurantHome from "@/components/restaurant Page/restaurantHome.vue";
+import RestaurantMenu from "@/components/restaurant Page/restaurantMenu.vue";
+import RestaurantContact from "@/components/restaurant Page/restaurantContact.vue";
 
 // HomeView == 0, menuView == 1, contactView == 2
 const activeView = ref([
@@ -21,7 +32,7 @@ const toggleView = (arg: number) => {
 //   0 == HomeView
 //   1 == menuView
 //   2 == contactView
-  for (let i = 0; i < activeView.value.length; i++){
+  for (let i = 0; i < activeView.value.length; i++) {
     activeView.value[i] = false
   }
   activeView.value[arg] = true
@@ -35,5 +46,27 @@ const toggleView = (arg: number) => {
 <style scoped>
 .restaurantBody {
   padding: 50px 50px;
+  height: 100vh;
+  width: 100vw;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+.home, .menu, .contact {
+  position: absolute;
+  left: 0;
+  top: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
